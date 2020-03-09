c46ee9bb446025787329724bb49d49820707180e
junyorstan <vstancisco@gmail.com>
2020-03-09 16:58:40 +0200

    added images to products page and footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
 class Product extends Component {
     render() {
+        const {id,title,img,price,inCart} = this.props.product;
         return (
-            <div>
-                <h3>Salut from Product</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={() => console.log('ive been clicked')}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"/>
+                        </Link>
+                        <button className="cart-btn" disabled={inCart?true:false} onClick={() => {console.log('added to cart');}}>
+
+                        {inCart?(<p className="text-capitalize  mb-0" disabled>in Cart</p>):(<i className="fa fa-cart-plus"/>)}
+
+                        </button>
+                    </div>
+
+                    {/*card footer*/}
+                    https://youtu.be/wPQ1-33teR4?t=7880
+                </div>
+            </ProductWrapper>
         );
     }
 }
+
+const ProductWrapper = styled.div`
+
+`;
 
 export default Product;
